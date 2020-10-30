c1dfcf0b1b27e16ff0b27dc7ee71f0cf5cf113c5
Michael Truong <michaeltruong@Michaels-MacBook-Air.local>
2020-10-29 21:19:47 -0400

    completed up to some question

--- array-methods-callbacks/solutions.js
+++ array-methods-callbacks/solutions.js
@@ -5,24 +5,79 @@ const panagram = ['The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy'
 
 // Every
 // Determine if every number is greater than or equal to 0
-nums.forEach( (n) => { 
-    if (n >= 0) {
-        console.log(n + " is greater than or equal to 0");
-    } else {
-        console.log(n + " is not greater than 0");
-    }
+const a = nums.every( (n) => { 
+    return (n >= 0);
 }); 
+console.log(a); 
 
 // determine if every word shorter than 8 characters
-panagram.forEach( (w) => {
-    if (w.length < 8) {
-        console.log(w + " is shorter than 8 chararacters");
-    } else {
-        console.log(w + " is not shorter than 8 chararacters");
-    }
+const b = panagram.every( (w) => {
+    return (w.length < 8);
 }); 
-
+console.log(b);
 // Filter 
 // Filter the array for numbers less than 4
+const c = nums.filter( (n) => {
+    return (n < 4); 
+});
+console.log(c);
 
 // Filter the words than have an even length 
+const d = panagram.filter( (w) => { 
+    return (w.length % 2 === 0); 
+});
+console.log(d); 
+
+// Find
+// Find the first value divisible by 5
+const e = nums.find( (n) => {
+    return (n % 5 ===0);
+});
+console.log(e);
+
+// find the first word that is longer than 5 characters
+const f = panagram.find( (w) => {
+    return (w.length > 5); 
+});
+console.log(f); 
+
+// Find Index 
+// find the index of the first number that is divisible by 3
+const g = nums.findIndex((n) => {
+    return (n % 3 === 0);
+}); 
+console.log(g); 
+
+// find the index of the first word that is less than 2 characters long
+const h = panagram.findIndex( (w) => {
+    return (w.length < 2); 
+});
+console.log(h);
+
+// For Each 
+// console.log each value of the nums array multiplied by 3
+nums.forEach( (n) => {
+    console.log(n * 3);
+    return;
+}); 
+
+// console.log each word with an exclamation point at the end of it
+panagram.forEach ( (w) => {
+    console.log( w + "!");
+    return;
+});
+
+// Map
+// make a new array of each number multiplied by 100
+const i = nums.map((n) => {
+    return (n*100); 
+});
+console.log(i); 
+
+// make a new array of all the words in all uppercase
+const j = panagram.map( (w) => {
+    return w.toUpperCase();
+});
+console.log(j);
+
+// Some 
